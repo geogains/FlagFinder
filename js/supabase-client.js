4f20865fc53eb74e7a699e4795504c7a2fae1d8b
Kieron Crooks <geogains25@gmail.com>
2026-02-05 21:29:13 +0400

    Update v2.2 ⚙️

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,7 +1,7 @@
 // js/supabase-client.js
 import { createClient } from 'https://esm.sh/@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://api.geo-ranks.com';
+const SUPABASE_URL = 'https://ajwxgdaninuzcpfwawug.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqd3hnZGFuaW51emNwZndhd3VnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1MDI5ODgsImV4cCI6MjA3NzA3ODk4OH0._LvYsqhSZIsWLIvAYtEceg1fXbEuaM0DElY5poVqZxI';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
